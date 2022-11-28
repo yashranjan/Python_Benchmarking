8c45d20570588b39037f2c986b3ec4cc80928ee2
Yash Ranjan <yashranjan74@gmail.com>
2022-11-28 23:48:37 +0530

    Initial commit, setup benchmarking

---
+++ parse_xml.py
@@ -0,0 +1,54 @@
+import subprocess
+import re
+loop, repeat, unit = 100, 100, 'msec'
+
+class Point:
+    def __init__(self, py='3.6', loop=0, repeat=0, time=0.0) -> None:
+        self.py = py
+        self.loop = loop
+        self.repeat = repeat
+        self.time = time
+
+class DataPoint:
+    def __init__(self, code='') -> None:
+        self.code = code
+        self.py_data = []
+    
+    def addPoint(self, py, loop, repeat, time):
+        self.py_data.append(Point(py, loop, repeat, time))
+    
+    def __repr__(self) -> str:
+        return 'For TestCode: {}\n {}\n'.format(self.code, '\n'.join(['{idx}. {i.py}=> Loops:{i.loop}, Rep:{i.repeat}, Time per loop:{i.time}{unit}'.format(idx=idx, i=i, unit=unit) for idx, i in enumerate(self.py_data)]))
+
+def main():
+    pat = r'[a-zA-Z0-9, ]+: [0-9]*.[0-9]*'
+    
+    code1 = '''python3.{ver} -m timeit -n {loop} -r {repeat} -u {unit} "import xml.etree.ElementTree as ET;tree = ET.parse('books.xml');"'''
+    code2 = '''python3.{ver} -m timeit -n {loop} -r {repeat} -u {unit} "import xml.etree.ElementTree as ET;tree = ET.parse('mega.xml');"'''
+
+    data_dict = {
+        'test_1':DataPoint(code1),
+        'test_2':DataPoint(code2)
+    }
+
+    for test, test_data in data_dict.items():
+        code = test_data.code
+        for ver in ['6', '8', '9', '10', '11']:
+            py_ver = 'Py3.{}'.format(ver)
+            ver_code = code.format(ver=ver, loop=loop, repeat=repeat, unit=unit)
+            
+            res = subprocess.check_output(ver_code, shell=True)
+            res = res.decode('utf-8')
+
+            pat_res = re.findall(pat, res)
+            if pat_res:
+                time = pat_res[0].split(': ')[1]
+                point_obj = Point(py_ver, loop, repeat, time)
+                test_data.py_data.append(point_obj)
+
+    for key, item in data_dict.items():
+        print(item)
+
+
+if __name__ == '__main__':
+    main()
