b0d3f046954131f26d0d92c313765472863eb512
Yash Ranjan <yashranjan74@gmail.com>
2022-12-01 14:49:33 +0530

    Better logging

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-CURR_DATETIME=$(shell date)
+CURR_DATETIME ?= $(shell date)
+
+loop ?= 4
+repeat ?= 3
 
 gen_res:
-	python3 parse_xml.py > res.txt
+	python3 parse_xml.py loop=$(loop) repeat=$(repeat) > res.txt
 	cat res.txt 
 
 test:
--- parse_xml.py
+++ parse_xml.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 import time
+import sys
 
 pat = r'[a-zA-Z0-9, ]+: [0-9]*.[0-9]*'
 loop, repeat, unit = 1, 1, 'sec'
@@ -21,7 +22,7 @@ class DataPoint:
         self.py_data.append(Point(py, loop, repeat, time))
     
     def __repr__(self) -> str:
-        return 'For TestCode: {}\n {}\n'.format(self.code, '\n'.join(['{idx}. {i.py}=> Loops:{i.loop}, Rep:{i.repeat}, Time per loop:{i.time} {unit}'.format(idx=idx, i=i, unit=unit) for idx, i in enumerate(self.py_data)]))
+        return 'For TestCode: {}\n {}\n'.format(self.code, '\n'.join(['{idx}. {i.py}=> Loops:{i.loop}, Rep:{i.repeat}, Min. Time per loop:{i.time} {unit}'.format(idx=idx, i=i, unit=unit) for idx, i in enumerate(self.py_data)]))
 
 def test_func(ver_code, py_ver, test_data):
     res = subprocess.check_output(ver_code, shell=True)
@@ -30,9 +31,10 @@ def test_func(ver_code, py_ver, test_data):
     if pat_res:
         time = pat_res[0].split(': ')[1]
         test_data.addPoint(py_ver, loop, repeat, time)
+        print('Test done in {}{}!!'.format(time, unit))
 
 def main():
-
+    global loop, repeat
     timeit_tmp = 'python3.{ver} -m timeit -n {loop} -r {repeat} --unit={unit}'
 
     code1 = "import xml.etree.ElementTree as ET;tree = ET.parse('books.xml');"
@@ -49,6 +51,7 @@ def main():
         for ver in ['6', '8', '9', '10', '11']:
             py_ver = 'Py3.{}'.format(ver)
             ver_code = code.format(ver=ver, loop=loop, repeat=repeat, unit=unit)
+            print('Initiating benchmark {} for {}!!'.format(test, py_ver))
             test_func(ver_code, py_ver, test_data)
 
     for key, item in data_dict.items():
@@ -56,6 +59,11 @@ def main():
 
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        options_dict = dict([i.split('=') for i in sys.argv[1:]])
+        loop = int(options_dict.get('loop', loop))
+        repeat = int(options_dict.get('repeat', repeat))
     st = time.time()
+    print('Starting tests with loops={} and repeat={}!!'.format(loop, repeat))
     main()
     print('Total Time Elapsed in main: {}s'.format(time.time() - st))
